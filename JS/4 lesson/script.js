cf904f1540df5734a6d228ee92b1f95152966568
Maksim_Palyshenkau <makatan19@gmail.com>
2020-06-08 17:26:34 +0300

    js4

---
+++ JS/4 lesson/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      return confirm('А родители разрешили?');
+    }
+  }
+  
+  let age = prompt('Сколько вам лет?', 18);
+  
+  if ( checkAge(age) ) {
+    alert( 'Доступ получен' );
+  } else {
+    alert( 'Доступ закрыт' );
+  }
