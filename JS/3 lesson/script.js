378b89559df6abff126c2f8f4a663260f7767b0c
makatan19 <makatan19@gmail.com>
2020-06-13 00:59:50 +0300

    js3

---
+++ JS/3 lesson/script.js
@@ -0,0 +1,7 @@
+let age = prompt ('Сколько лет?', 100);
+alert(`Тебе ${age} лет!`);
+let isBoss = confirm('Ты здесь главный?');
+alert(isBoss+'кууу');
+
+let accsess = (age>18)? true : false;
+alert (accsess);
