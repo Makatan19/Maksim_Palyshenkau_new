f5d0c63de9d7f984d0feaec89c0294b363b1db66
Maksim_Palyshenkau <makatan19@gmail.com>
2020-08-06 19:53:12 +0300

    imges

---
+++ Maket/scripts/script_about_us.js
@@ -0,0 +1,71 @@
+function click__img1() {
+    var EL = document.getElementById('gallery__img1');
+    EL.style.display = "block";
+}
+function close_img1() {
+    var EL = document.getElementById('gallery__img1');
+    EL.style.display = "none";
+}
+
+function click__img2() {
+    var EL = document.getElementById('gallery__img2');
+    EL.style.display = "block";
+}
+function close_img2() {
+    var EL = document.getElementById('gallery__img2');
+    EL.style.display = "none";
+}
+
+function click__img3() {
+    var EL = document.getElementById('gallery__img3');
+    EL.style.display = "block";
+}
+function close_img3() {
+    var EL = document.getElementById('gallery__img3');
+    EL.style.display = "none";
+}
+
+function click__img4() {
+    var EL = document.getElementById('gallery__img4');
+    EL.style.display = "block";
+}
+function close_img4() {
+    var EL = document.getElementById('gallery__img4');
+    EL.style.display = "none";
+}
+
+function click__img5() {
+    var EL = document.getElementById('gallery__img5');
+    EL.style.display = "block";
+}
+function close_img5() {
+    var EL = document.getElementById('gallery__img5');
+    EL.style.display = "none";
+}
+
+function click__img6() {
+    var EL = document.getElementById('gallery__img6');
+    EL.style.display = "block";
+}
+function close_img6() {
+    var EL = document.getElementById('gallery__img6');
+    EL.style.display = "none";
+}
+
+function click__img7() {
+    var EL = document.getElementById('gallery__img7');
+    EL.style.display = "block";
+}
+function close_img7() {
+    var EL = document.getElementById('gallery__img7');
+    EL.style.display = "none";
+}
+
+function click__img8() {
+    var EL = document.getElementById('gallery__img8');
+    EL.style.display = "block";
+}
+function close_img8() {
+    var EL = document.getElementById('gallery__img8');
+    EL.style.display = "none";
+}
